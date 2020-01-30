1e7dff3ba606abc6719d8a8e85dea5b04459c3cc
JoëlPierreDupont <36246708+joelpierredupont@users.noreply.github.com>
2020-01-30 11:47:24 +0100

    Update Bat.bat

--- Bat.bat
+++ Bat.bat
@@ -1,15 +1,15 @@
 @ECHO OFF
 
 REM Definition des variables
-set a=milenium
-set c=milenium
+set a=*****
+set c=*****
 
 REM Config de la taille
 set NLS_LANG=FRENCH_FRANCE.WE8PC850
 mode CON: COLS=201 LINES=50
 title Titre de mn
 
-REM Execution de la requête sur la base avec SQLPLUS
+REM Execution de la requÃªte sur la base avec SQLPLUS
 sqlplus %a%/%b%@mabase @"Bat.sql"
 
 REM Pause

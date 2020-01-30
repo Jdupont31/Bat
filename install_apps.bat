a67085472b3c999862de014c7f2877c016bd980f
JoëlPierreDupont <36246708+joelpierredupont@users.noreply.github.com>
2020-01-30 11:49:36 +0100

    Update install_apps.bat

--- install_apps.bat
+++ install_apps.bat
@@ -1,11 +1,11 @@
 
 echo Installation LibreOffice
 echo ================================================
-start /wait msiexec /qb-! /i "C:\Users\dupont\Desktop\LibO_3.6.4_Win_x86_install_multi.msi" ALLUSER=1 SELECT_WORD=1 SELECT_EXCEL=1 SELECT_POWERPOINT=1 USERNAME=ddm COMPANYNAME=ddm
+start /wait msiexec /qb-! /i "C:\Users\dupont\Desktop\LibO_3.6.4_Win_x86_install_multi.msi" ALLUSER=1 SELECT_WORD=1 SELECT_EXCEL=1 SELECT_POWERPOINT=1 USERNAME=moi COMPANYNAME=entreprise
 echo Termine
 echo.
 echo.
-:: /x désinstalle /i installe /comments
+:: /x dÃ©sinstalle /i installe /comments
 
 
 -----------------------------

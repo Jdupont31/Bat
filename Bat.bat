01774cc1de8bd01cf8c092af74e4b3fc3208c656
JoëlPierreDupont <36246708+joelpierredupont@users.noreply.github.com>
2020-01-30 11:47:43 +0100

    Update Bat.bat

--- Bat.bat
+++ Bat.bat
@@ -7,7 +7,7 @@ set c=*****
 REM Config de la taille
 set NLS_LANG=FRENCH_FRANCE.WE8PC850
 mode CON: COLS=201 LINES=50
-title Titre de mn
+title Titre
 
 REM Execution de la requête sur la base avec SQLPLUS
 sqlplus %a%/%b%@mabase @"Bat.sql"
